4dc33fbc1e98e7be2aa2a99bc23317da247eca5e
Vikrant Biswas <vikrant.biswas@gmail.com>
2017-02-02 15:30:23 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget --no-check-certificate https://gist.githubusercontent.com/windlessStorm/aa40ee62007dfa9a6af421b6d3a12b2c/raw/86dd804d6a5c2ab37be54f9645a4b694bd599c84/bashrc bashrc
+wget https://github.com/windlessStorm/My-Bashrc/blob/master/bashrc bashrc
 cat bashrc > .bashrc
 rm -f bashrc
 rm -- "$0"
