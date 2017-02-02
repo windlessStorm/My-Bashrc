1d78ee0b984a359d37f2193d42ceb811077e40e7
Vikrant Biswas <vikrant.biswas@gmail.com>
2017-02-02 15:32:47 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-wget https://github.com/windlessStorm/My-Bashrc/blob/master/bashrc bashrc
-cat bashrc > .bashrc
-rm -f bashrc
-rm -- "$0"
-source .bashrc
-rm -- "$0"
-echo "Done!" 
+cat bashrc > ~/.bashrc
+source ~/.bashrc
+echo "Done!"
